88ed8081c88a2f9850873972673bdc18f9e14096
Uli Köhler <ulikoehler@online.de>
2013-04-24 04:32:20 +0200

    Added log stub

---
+++ ZKVStore/Log.cpp
@@ -0,0 +1,18 @@
+/* 
+ * File:   Log.cpp
+ * Author: uli
+ * 
+ * Created on 24. April 2013, 04:29
+ */
+
+#include "Log.hpp"
+
+Log::Log() {
+}
+
+Log::Log(const Log& orig) {
+}
+
+Log::~Log() {
+}
+
---
+++ ZKVStore/Log.hpp
@@ -0,0 +1,32 @@
+/* 
+ * File:   Log.hpp
+ * Author: uli
+ *
+ * Created on 24. April 2013, 04:29
+ */
+
+#ifndef LOG_HPP
+#define	LOG_HPP
+
+class Log {
+public:
+    Log();
+    Log(const Log& orig);
+    virtual ~Log();
+private:
+
+};
+
+
+class LogServer {
+public:
+    Log();
+    Log(const Log& orig);
+    virtual ~Log();
+private:
+
+};
+
+
+#endif	/* LOG_HPP */
+
