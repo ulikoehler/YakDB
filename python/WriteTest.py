0815a89f2dee39bcfbbf4506f852df12e98c3c95
Uli Köhler <ulikoehler@online.de>
2013-07-09 19:26:38 +0200

    Fix ZeroDB write API

--- python/WriteTest.py
+++ python/WriteTest.py
@@ -1,13 +1,15 @@
 import zerodb
 import random
+import zmq
 
 #Create a dataset of 1k string KV pairs
 data = {}
-for i in range(1,1000):
+for i in range(1,10000):
     data[str(i)] = str(random.random())
 
 conn = zerodb.ZeroDBConnection()
 conn.connect("tcp://localhost:7100")
+
 conn.writeDict(0, data)
 
 print conn.serverInfo()
--- python/zerodb.py
+++ python/zerodb.py
@@ -2,11 +2,11 @@ import zmq
 import struct
 
 #TODO document these
-class ParameterError(Exception):
+class ParameterException(Exception):
     def __init__(self, message, Errors):
         Exception.__init__(self, message)
 
-class ValidationError(Exception):
+class ZeroDBProtocolException(Exception):
     def __init__(self, message, Errors):
         Exception.__init__(self, message)
 
@@ -58,7 +58,7 @@ class ZeroDBConnection:
         """
         if type(value) is not int:
             raise Exception("Can't format object of non-integer type as binary integer")
-        self.socket.send(struct.pack('<I', value))
+        self.socket.send(struct.pack('<I', value), zmq.SNDMORE)
     def serverInfo(self):
         """
         Send a server info request to the client
@@ -82,23 +82,35 @@ class ZeroDBConnection:
         @param tableNo The numeric, unsigned table number to write to
         @param valueDict A dictionary containing the key/value pairs to be written.
                         Must not contain None keys or values.
-                        integral types are automatically mapped to little-endian binary,
-                        floating point types are mapped to little-endian 64-bit IEEE754 values.
+                        integral types are automatically mapped to signed 32-bit little-endian binary,
+                        floating point types are mapped to signed little-endian 64-bit IEEE754 values.
                         If you'd like to use another binary representation, use a binary string instead!
         @param partsync If set to true, subsequent reads are guaranteed to return the written values
         @param fullsync If set to true, written data is synced to disk after being written.
+        @return True on success, else an appropriate exception will be raised
         """
         #Check parameters
         if type(tableNo) is not int:
-            raise ParameterError("Table number parameter is not an integer!")
+            raise ParameterException("Table number parameter is not an integer!")
         #Check if this connection instance is setup correctly
         self._checkConnection()
+        #Before sending any frames, check the value dictionary for validity
+        #Else, the socket could be left in an inconsistent state
+        if len(valueDict) is 0:
+            raise ParameterException("Dictionary to be written did not contain any valid data!")
+        for key in valueDict.iterkeys():
+            value = valueDict[key]
+            #None keys or values are not supported, they can't be mapped to binary!
+            # Use empty strings if neccessary.
+            if key is None:
+                raise ParameterException("'None' keys are not supported by ZeroDB!")
+            if value is None:
+                raise ParameterException("'None' values are not supported by ZeroDB!")
         #Send header frame
         flags = 0
         if partsync: flags |= 1
         if fullsync: flags |= 2
-        headerStr = "\x31\x01\x20"
-        headerStr += chr(flags)
+        headerStr = "\x31\x01\x20" + chr(flags)
         self.socket.send(headerStr, zmq.SNDMORE)
         #Send the table number
         self._sendBinary32(tableNo)
@@ -106,23 +118,29 @@ class ZeroDBConnection:
         nextToSend = None #Needed because the last value shall be sent w/out SNDMORE
         for key in valueDict.iterkeys():
             #Send the value from the last loop iteration
-            if nextToSend is not None: self.socket.send(key, zmq.SNDMORE)
-            #Check if key is mappable-to-binary data
-            if key is None:
-                raise Exception("'None' keys are not supported by ZeroDB!")
+            if nextToSend is not None: self.socket.send(value, zmq.SNDMORE)
+            #Map key to binary data if neccessary
             value = valueDict[key]
-            #None values are not supported, they can't be mapped to binary!
-            # Use empty strings if neccessary.
-            if value is None:
-                raise Exception("'None' values are not supported by ZeroDB!")
+            if type(value) is int:
+                value = struct.pack('<i', value)
+            if type(value) is float:
+                value = struct.pack('<d', value)
             #Send the key and enqueue the value
             self.socket.send(key, zmq.SNDMORE)
             nextToSend = value
         #If nextToSend is None now, the dict didn't contain valid data
-        if nextToSend is None:
-            raise Exception("Dictionary to be written did not contain any valid data!")
         #Send the last value without SNDMORE
         self.socket.send(nextToSend)
+        #If this is a req/rep connection, receive a reply
+        if self.mode is zmq.REQ:
+            msgParts = self.socket.recv_multipart(copy=True)
+            if len(msgParts) == 0:
+                raise ZeroDBProtocolException("Received empty reply message")
+            if msgParts[0][2] != '\x20':
+                raise ZeroDBProtocolException("Put response code was %d instead of 0x20" % msgParts[0][2])
+            if msgParts[0][3] != '\x00':
+                raise ZeroDBProtocolException("Put response status code was %d instead of 0x00 (ACK)" % msgParts[0][3])
+        return True
     def __del__(self):
         """Cleanup ZMQ resources. Auto-destroys context if none was given"""
         if self.socket is not None:
