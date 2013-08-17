3f607f4d38cae33079efb39a19847a4b2d665d44
Uli Köhler <ulikoehler@online.de>
2013-08-17 18:23:18 +0200

    Added mapper declarations

--- mapred/mapper.h
+++ mapred/mapper.h
@@ -1,33 +1,41 @@
+#ifndef __MAPPER_H
+#define __MAPPER_H
+
 #include <string>
+#include <map>
+#include <yakclient/YakClient.hpp>
+#include <yakclient/Batch.hpp>
+
+//initialize(), map(), and cleanup() must implement this
+#define YAKEXPORT extern "C"
 
 /**
- * A data sink for MapReduce applications.
- * Only one thread may write to this.
- * Not synchronized.
- * 
- * Provides a simple Key-Value interface to a database.
- * 
- * Auto-batches writes.
+ * This function is called once on initialization.
+ * @param connection A fast connection to the server.
+ *              Usually this is used to write data to the output table.
+ * @param parameters The parameter map.
+ *      The "outputTable" parameter contains the configured output table.
  */
-class DataSink {
-public:
-    /**
-     * Construct a new data sink.
-     * 
-     * @param socket A ZeroMQ REQ/REP socket.
-     *  Must be already connected, won't be closed from inside this class
-     * @param batchSize The batch size for autobatching
-     */
-    DataSink(void* socket, size_t batchSize=1000);
-    int write();
-    /**
-     * Destructs the current DataSink instance.
-     * Automatically flushes the current batch if neccessary.
-     */
-    ~DataSink();
-private:
-public:
-    void* socket;
-    size_t currentSize;
-    size_t batchSize;
-}
+void YAKEXPORT initialize(YakConnection& connection,
+                          const std::map<std::string, std::string>& parameters);
+
+/**
+ * This function is called for every map key.
+ * @parameter key The key. The application may modify this without restrictions,
+ *      but not access more than keyLength bytes.
+ * @param
+ * @parameter value The value, corresponding to the key. The application may modify 
+ *      this without restrictions, but not access more than keyLength bytes.
+ */
+void YAKEXPORT map(char* key, size_t keyLength, char* value, size_t valueLength);
+
+/**
+ * This is called once when the last key has been read.
+ * It shall cleanup the application state.
+ * 
+ * The map job might be deallocated after this is called,
+ * or initialize() might be called again.
+ */
+void YAKEXPORT cleanup(void);
+
+#endif //__MAPPER_H
