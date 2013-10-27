86b631520d213eb0c13d5d91cbadfca3c1dc1329
Uli Köhler <ulikoehler@online.de>
2013-10-27 03:22:40 +0100

    Added DB dumper (python), untested yet

--- python/YakDB/Dump.py
+++ python/YakDB/Dump.py
@@ -4,11 +4,34 @@
 Database dump / dump import for YakDB
 Provides utility functions for dumping
 """
+from __future__ import with_statement
 import struct
+import gzip
+from YakDB.Batch import AutoWriteBatch
 
-keyValueMagicByte = 0x6DE0
+__keyValueMagicByte = 0x6DE0
+__headerMagicByte = 0x6DDF
+__headerVersionByte = 0x0001
 
-def __writeKeyValue(f, key, value):
+def __writeYDFFileHeader(f):
+    """
+    Write the YDF file header to a file-like object
+    """
+    f.write(struct.pack("<HH", __headerMagicByte, __headerVersionByte))
+
+def __verifyYDFFileHeader(f):
+    """
+    Read a YDF file header from a given file-like object,
+    verify it and throw an exception if it doesn't match
+    """
+    hdr = f.read(4)
+    magicByte, version = struct.unpack("<HH", hdr)
+    if magicByte != __headerMagicByte:
+        raise ValueError("YDF file header magic word mismatches, expected %d but was %d" % (__headerMagicByte, magicByte))
+    if version != __headerVersionByte:
+        raise ValueError("YDF file header version mismatches, expected %d but was %d" % (__headerVersionByte, version))
+
+def __writeYDFKeyValue(f, key, value):
     """
     Append a single key-value record to a file-like object
     
@@ -17,13 +40,54 @@ def __writeKeyValue(f, key, value):
         key -- The key to write, in any binary writable form (not unicode)
         value -- The value to write, in any binary writable form (not unicode)
     """
-    hdr = struct.pack("<HQQ", keyValueMagicByte, len(key), len(value))
+    hdr = struct.pack("<HQQ", __keyValueMagicByte, len(key), len(value))
     f.write(hdr)
     f.write(key)
     f.write(value)
 
-def dump(conn, tableNo, startKey=None, endKey=None, limit=None, keyFilter=None, valueFilter=None, chunkSize=1000):
+def __readYDFKeyValue(f):
     """
-    Dump a table by using a snapshotted table version.
+    Read a YDF-formatted key-value block.
+    
+    Return value: A tuple (key, value) or None if there is no record left
     """
-    initializePassiveDataJob(conn, 
+    kvHdr = f.read(18) #2 magic word + 8 key size + 8 value size
+    #Check if any data could be read
+    if len(kvHdr) == 0: return None
+    #Unpack binary format and check header
+    magicByte, keySize, valueSize = struct.unpack("<HQQ", kvHdr)
+    if magicByte != __keyValueMagicByte:
+        raise ValueError("YDF Key-Value header magic word mismatches, expected %d but was %d" % (__keyValueMagicByte, magicByte))
+    key = f.read(keySize)
+    value = f.read(valueSize)
+    return (key, value)
+    
+
+def dump(conn, outputFilename, tableNo, startKey=None, endKey=None, limit=None, keyFilter=None, valueFilter=None, chunkSize=1000):
+    """
+    Dump a table to YDF by using a snapshotted table version.
+    """
+    job = initializePassiveDataJob(conn, startKey, endKey, limit, keyFilter, valueFilter, chunkSize)
+    #Transparent compression
+    openFunction = gzip.open if outputFilename.endswith(".gz") else open
+    with openFunction(outputFilename, "wb") as outfile:
+        __writeYDFFileHeader(outfile)
+        for key, value in job.iteritems():
+            __writeYDFKeyValue(outfile, key, value)
+
+def importDump(conn, inputFilename, tableNo):
+    """
+    Import a database dump in YDF format
+    """
+    #Auto-batch writes
+    batch = AutoWriteBatch(conn, tableNo)
+    #Transparent decompression
+    openFunction = gzip.open if inputFilename.endswith(".gz") else open
+    with openFunction(inputFilename, "rb") as infile:
+        __verifyYDFFileHeader(infile)
+        while True:
+            ret = __readYDFKeyValue(infile)
+            #None --> EOF
+            if ret is None: break
+            key, value = ret
+            batch.putSingle(key, value)
