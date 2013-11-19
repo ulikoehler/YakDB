6502ea9aa991a63904bfe4cb21bf5d29a5bcb52e
Uli Köhler <ulikoehler@online.de>
2013-11-19 05:02:31 +0100

    Started to implement dynamically-loaded UDP beacon driver

--- YakServer/include/ServerBeacon.hpp
+++ YakServer/include/ServerBeacon.hpp
@@ -9,13 +9,25 @@
 #define	SERVERBEACON_HPP
 #include <zmq.h>
 
+/**
+ * A CZMQ ZBeacon-based autodiscovery controller that loads CZMQ using the dynamic
+ * linker and therefore does not introduce a hard dependency on it.
+ * 
+ * 
+ */
 class ServerBeacon {
 public:
-    ServerBeacon(const std::string& clusterName, uint32_t interval = 1000);
+    ServerBeacon(void* context, const std::string& clusterName, uint32_t interval = 1000);
     void startBeacon(uint32_t intervalMsec);
     ~ServerBeacon();
 private:
-    zbeacon_t* beacon;
+    void* context;
+    std::string clusterName;
+    uint32_t interval;
+    /**
+     */
+    void* libczmq;
+    Logger logger;
 };
 
 #endif	/* SERVERBEACON_HPP */
--- YakServer/src/ServerBeacon.cpp
+++ YakServer/src/ServerBeacon.cpp
@@ -6,11 +6,36 @@
  */
 
 #include <string>
+//Dynamic linker
+#include <dlfcn.h>
 
 #include "ServerBeacon.hpp"
 
-ServerBeacon::ServerBeacon(const std::string& clusterName, uint32_t interval) : beacon(zbeacon_new(7007)) {
-    assert(*zbeacon_hostname(beacon));
+//Typedefs for dynamically loaded functions
+#define zbeacon_t void
+typedef zbeacon_t* (*zbeacon_new_t)(int /*port_nbr*/);
+typedef void (*zbeacon_destroy_t)(zbeacon_t** /*self_p*/);
+typedef void (*zbeacon_set_interval_t)(zbeacon_t* /* self*/, int /*interval*/);
+typedef void (*zbeacon_noecho_t)(zbeacon_t* /*self*/);
+typedef void (*zbeacon_publish_t)(zbeacon_t* /*self*/, byte* /*transmit*/, size_t /*size*/);
+typedef void (*zbeacon_silence_t)(zbeacon_t* /*self*/);
+typedef void (*zbeacon_subscribe_t)(zbeacon_t* /*self*/, byte* /*filter*/, size_t /*size*/);
+typedef void (*zbeacon_unsubscribe_t)(zbeacon_t* self);
+typedef void* (*zbeacon_socket_t)(zbeacon_t* self);
+
+ServerBeacon::ServerBeacon(void* context, const std::string& clusterName, uint32_t interval) : 
+    context(context),
+    clusterName(clusterName),
+    interval(interval),
+    logger(context, "UDP beacon") {
+    //Load the library 
+    libczmq = dlopen("libczmq.so", RTLD_LAZY);
+    char* dlopenError = dlerror();
+    if(unlikely(dlopenError != nullptr)) {
+        logger.error("Error while loading CZMQ for the UDP beacon: " + std::string(dlopenError));
+        return;
+    }
+    //assert(*zbeacon_hostname(beacon));
     zbeacon_noecho(beacon);
     zbeacon_set_interval(beacon, interval);
     std::string publishName = "ZKV/" + clusterName;
