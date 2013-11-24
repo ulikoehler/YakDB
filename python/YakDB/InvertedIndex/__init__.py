eab85f8bfde642afed6d966481f28cd335087953
Uli Köhler <ulikoehler@online.de>
2013-11-24 03:15:42 +0100

    Fix __init

--- python/YakDB/InvertedIndex/__init__.py
+++ python/YakDB/InvertedIndex/__init__.py
@@ -2,3 +2,5 @@
 # -*- coding: utf8 -*-
 
 __all__ = ["InvertedIndex","EntityInvertedIndex"]
+from YakDB.InvertedIndex.InvertedIndex import InvertedIndex 
+from YakDB.InvertedIndex.EntityInvertedIndex import EntityInvertedIndex 
