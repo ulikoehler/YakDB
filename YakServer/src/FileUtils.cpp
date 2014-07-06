4ee39bd475efe3dab49a268fd65f65f0813e0905
Uli Köhler <ulikoehler@online.de>
2014-07-07 00:18:17 +0200

    Added table filesize calculation

--- YakServer/src/FileUtils.cpp
+++ YakServer/src/FileUtils.cpp
@@ -4,6 +4,7 @@
 #include <sys/types.h>
 #include <sstream>
 #include <limits>
+#include <cstring>
 
 bool fileExists(const std::string& file) {
     struct stat buf;
@@ -20,15 +21,15 @@ uint64_t parseUint64(const std::string& value) {
     return ret;
 }
 
+#include <iostream>
+using namespace std;
 size_t getFilesize(const char* filename) {
     struct stat st;
-    if(stat(filename, &st) == 0) {
+    if(stat(filename, &st) != 0) {
         return std::numeric_limits<size_t>::max();
     }
     return st.st_size;   
 }
-#include <iostream>
-using namespace std;
 size_t getDirectoryFilesize(const char* dirname) {
     /**
      * Using boost::filesystem would make this task easier,
@@ -39,20 +40,23 @@ size_t getDirectoryFilesize(const char* dirname) {
     DIR* dir;
     struct dirent* ent;
     size_t filesizeSum = 0;
-    cout << "FREC " << dirname << endl;
     if ((dir = opendir(dirname)) != nullptr) {
         //Iterate over entries in directory
         while ((ent = readdir(dir)) != nullptr) {
+            //ent->d_name is relative to the current directory.
+            //Make it absolute
+            std::string absoluteFilename =
+                std::string(dirname) + "/" + std::string(ent->d_name);
             if(ent->d_type == DT_DIR) {
                 //Skip . and .. which lead to infinite recursion
                 if (strcmp(".", ent->d_name) == 0 || strcmp("..", ent->d_name) == 0) {
                     continue;
                 }
                 //Recurse into directory
-                filesizeSum += getDirectoryFilesize(ent->d_name);
+                filesizeSum += getDirectoryFilesize(absoluteFilename.c_str());
             } else if(ent->d_type == DT_REG) {
                 //Regular file -> use its size
-                size_t filesize = getFilesize(ent->d_name);
+                size_t filesize = getFilesize(absoluteFilename.c_str());
                 //getFilesize returns max() on error
                 if(filesize == std::numeric_limits<size_t>::max()) {
                     filesize = 0;
